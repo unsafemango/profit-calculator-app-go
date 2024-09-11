f51fb77a7ae98c8a92089a70ca549a619690508d
yray akemu <yrayak@outlook.com>
2024-09-11 00:04:23 -0400

    added input from user using fmt.scan()

--- app.go
+++ app.go
@@ -1,1 +1,27 @@
 package main
+
+import "fmt"
+
+func main() {
+	var revenue float64
+	var expenses float64
+	var taxRate float64
+
+	fmt.Print("Revenue: ")
+	fmt.Scan(&revenue)
+
+	fmt.Print("Expenses: ")
+	fmt.Scan(&expenses)
+
+	fmt.Print("TaxRate: ")
+	fmt.Scan(&taxRate)
+
+	ebt := revenue - expenses
+	fmt.Println(ebt)
+
+	profit := ebt * (1 - (taxRate/100))
+	fmt.Println(profit)
+
+	ratio := ebt / profit
+	fmt.Println(ratio)
+}
