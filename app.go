ccc766dab83fe2c18da438d319720f25a57ef312
yray akemu <yrayak@outlook.com>
2024-09-17 01:28:45 -0400

    added error logs and write calculated results to file

--- app.go
+++ app.go
@@ -1,35 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main() {
-	revenue := getUserInput("Revenue: ")
-	expenses := getUserInput("Expenses: ")
-	taxRate := getUserInput("TaxRate: ")
+	revenue, revenueErr := getUserInput("Revenue: ")
+	if revenueErr != nil {
+		fmt.Println("ERROR")
+		fmt.Println(revenueErr)
+		fmt.Println("----------")
+		panic("Can't continue, sorry")
+	}
+
+	expenses, expensesErr := getUserInput("Expenses: ")
+	if expensesErr != nil {
+		fmt.Println("ERROR")
+		fmt.Println(expensesErr)
+		fmt.Println("----------")
+		panic("Can't continue, sorry")
+	}
+
+	taxRate, taxRateErr := getUserInput("TaxRate: ")
+	if taxRateErr != nil {
+		fmt.Println("ERROR")
+		fmt.Println(taxRateErr)
+		fmt.Println("----------")
+		panic("Can't continue, sorry")
+	}
 
 	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
+
+	saveResultsToFile("ebt.txt", ebt)
+	saveResultsToFile("profit.txt", profit)
+	saveResultsToFile("ratio.txt", ratio)
 
 	formatOutput(ebt)
 	formatOutput(profit)
 	formatOutput(ratio)
 }
 
-func getUserInput(text string) float64 {
+func getUserInput(text string) (float64, error) {
 	var userInput float64
 	fmt.Print(text)
 	fmt.Scan(&userInput)
 
-	return userInput
+	if userInput <= 0 {
+		return 1, errors.New("Invalid input!")
+	}
+	return userInput, nil
 }
 
 func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
 	ebt := revenue - expenses
-	profit := ebt * (1 - (taxRate/100))
+	profit := ebt * (1 - (taxRate / 100))
 	ratio := ebt / profit
-	
+
 	return ebt, profit, ratio
 }
 
-func formatOutput(value float64){
+func formatOutput(value float64) {
 	fmt.Printf("%.2f\n", value)
+}
+
+func saveResultsToFile(fileName string, value float64) {
+	valueText := fmt.Sprint(value)
+	os.WriteFile(fileName, []byte(valueText), 0644)
 }
